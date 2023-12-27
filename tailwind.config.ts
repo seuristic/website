ffc961d3764a39a61815d0dec64ad365ab703961
M. Shahanwaz <57594421+mshahanwaz@users.noreply.github.com>
2023-12-27 19:45:14 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,5 @@
 import type { Config } from "tailwindcss";
-import colors from "tailwindcss/colors";
+const colors = require("tailwindcss/colors");
 
 const config: Config = {
   content: ["./app/**/*.{js,ts,jsx,tsx,mdx}"],
