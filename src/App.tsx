d6a6219e29b8b527b4c746c6fdd4baafb67ebe3c
Mohammad Shahanwaz <mohdshahanwaz55@gmail.com>
2025-10-29 18:03:59 +0530

    feat: implement lazy loading for Home and Photography pages with Suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
+import { Suspense, lazy } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Navbar } from '@/components/navbar'
-import Photography from '@/pages/Photography'
-import Home from '@/pages/Home'
+
+const Home = lazy(() => import('@/pages/Home'))
+const Photography = lazy(() => import('@/pages/Photography'))
 
 const App = () => {
   return (
     <Router>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        {/* <Route path="/blogs" element={<Blogs />} /> */}
-        <Route path="/photography" element={<Photography />} />
-      </Routes>
+      <Suspense
+        fallback={
+          <div className="bg-background text-foreground flex min-h-[calc(100vh-69px)] items-center justify-center text-sm">
+            Loading...
+          </div>
+        }
+      >
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/blogs" element={<Blogs />} /> */}
+          <Route path="/photography" element={<Photography />} />
+        </Routes>
+      </Suspense>
     </Router>
   )
 }
